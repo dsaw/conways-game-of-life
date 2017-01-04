6a41033a586551e9a9766e5a283feaa63b526422
Devesh Sawant <devesh47cool@gmail.com>
2017-01-04 23:36:57 +0530

    graphics basic testes

--- src/gameoflife/GameOfLifeBoard.java
+++ src/gameoflife/GameOfLifeBoard.java
@@ -5,19 +5,70 @@
  */
 package gameoflife;
 
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Devesh
  */
-public class GameOfLifeBoard {
-
+class GridSurface extends JPanel
+{
+    GridSurface()
+    {
+        
+    }
     
+    private void doDrawing(Graphics g)
+    {
+         Graphics2D g2d = (Graphics2D)g;
+         g2d.drawOval(20, 100, 40, 50);
+    
+    }
+    
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        doDrawing(g);
+        
+       }
+}
+
+
+
+
+
+public class GameOfLifeBoard extends JFrame {
+
+    GameOfLifeBoard()
+    {
+        initUI();
+    }
+    
+    
+    public void initUI()
+    {
+        add( new GridSurface());
+        
+        setSize(500,500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    } 
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //lambda expression used
+        EventQueue.invokeLater(() -> { 
+                                    GameOfLifeBoard board = new GameOfLifeBoard();
+                                    board.setVisible(true);
+                } );
     }
     
 }
